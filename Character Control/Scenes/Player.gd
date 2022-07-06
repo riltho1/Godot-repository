289424f3a9edf0f7891f6c30200c313134f38f02
riltho1 <100237088+riltho1@users.noreply.github.com>
2022-07-06 13:21:49 +1200

    Character controls working

--- Character Control/Scenes/Player.gd
+++ Character Control/Scenes/Player.gd
@@ -15,13 +15,29 @@ enum state {IDLE, RUNNING, PUSHING, ROLLING, STARTJUMP, JUMP, FALL, ATTACK}
 onready var player_state = state.IDLE
 
 func _ready():
-	$AnimationPlayer.play("Idle")
+	$AnimationPlayer.play("idle")
 	pass
 
 func update_animation(anim):
-	$AnimationPlayer.play(anim)
+	match(anim):
+		state.FALL:
+			$AnimationPlayer.play("fall")
+		state.ATTACK:
+			$AnimationPlayer.play("attack")
+		state.IDLE:
+			$AnimationPlayer.play("idle")
+		state.JUMP:
+			$AnimationPlayer.play("jump")
+		state.PUSHING:
+			$AnimationPlayer.play("pushing")
+		state.RUNNING:
+			$AnimationPlayer.play("running")
+	pass
 	
-func handle_state(state):
+func handle_state(player_state):
+	match(player_state):
+		state.STARTJUMP:
+			velocity.y = jump_speed
 	pass
 	
 func get_input():
@@ -33,9 +49,10 @@ func get_input():
 
 func _physics_process(delta):
 	get_input()
-	if velocity.x == Vector2.ZERO:
+	print(is_on_floor())
+	if velocity == Vector2.ZERO:
 		player_state = state.IDLE
-	elif velocity.x != 0 and Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		player_state = state.STARTJUMP
 	elif velocity.x != 0:
 		player_state = state.RUNNING
@@ -45,3 +62,9 @@ func _physics_process(delta):
 			state.JUMP
 		if velocity.y >0:
 			player_state = state.FALL
+			
+	handle_state(player_state)
+	update_animation(player_state)
+	#set gravity
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
