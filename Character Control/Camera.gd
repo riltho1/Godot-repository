3d9d350ed5a24239c34de6c6785365a9bcc65c55
riltho1 <100237088+riltho1@users.noreply.github.com>
2022-07-27 12:26:18 +1200

    Camera working

--- Character Control/Camera.gd
+++ Character Control/Camera.gd
@@ -8,6 +8,19 @@ var center = Vector2.ZERO
 func _ready():
 	center = get_viewport_rect().size/2
 	
-func _proccess(delta):
+func _process(delta):
 	if Input.is_action_just_released("zoom"):
-		pass
+		if zoomed:
+			target = null
+			zoomed = false
+		else:
+			target = owner.get_node("Player")
+			zoomed = true
+	
+	if zoomed:
+		zoom = zoom.move_toward(Vector2(0.3,0.3), 0.03)
+		position = position.move_toward(target.global_position,70)
+	
+	else:
+		zoom = zoom.move_toward(Vector2(1,1), 0.03)
+		position = position.move_toward(center,70)
