e973edd3fac4e9be423fd2c4c7cb81c362c779f8
riltho1 <100237088+riltho1@users.noreply.github.com>
2022-08-12 12:24:54 +1200

    Finished checkpoint and reset

--- Character Control/Scripts/SpinningBlade.gd
+++ Character Control/Scripts/SpinningBlade.gd
@@ -5,3 +5,11 @@ func _ready():
 	
 func _process(delta):
 	$Path2D/PathFollow2D.offset += 50 * delta
+
+
+func _on_Area2D_body_entered(body):
+	print(body)
+	if body.is_in_group("Player"):
+		print("player dead")
+		if GameStats.check_reset() == false:
+			body.global_position = GameStats.get_spawn().global_position
