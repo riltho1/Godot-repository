f1a4d49b37659aca2c108ed89e5831a5463d2182
riltho1 <100237088+riltho1@users.noreply.github.com>
2022-08-16 14:58:20 +1200

    Timer finished working on errors and fixes

--- Character Control/Scripts/World.gd
+++ Character Control/Scripts/World.gd
@@ -8,4 +8,7 @@ func _process(delta):
 	pass
 	if Input.is_action_just_pressed("reset"):
 		GameStats.reset()
-	$CanvasLayer/Control/Label.text = "TEXT"
+	$CanvasLayer/Control/Label.text = GameStats.get_time()
+
+func _on_Area2D_body_entered(body):
+	pass
