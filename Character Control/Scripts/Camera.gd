1343ab29c46af28665e1f58b9766209d32334e5e
riltho1 <100237088+riltho1@users.noreply.github.com>
2022-08-22 12:24:00 +1200

    Waterfall added and Camera edited as well as new tiles added

--- Character Control/Scripts/Camera.gd
+++ Character Control/Scripts/Camera.gd
@@ -1,12 +1,15 @@
 extends Camera2D
 
 var target = null
-var zoomed = false
+var zoomed = true
 var center = Vector2.ZERO
 
 
 func _ready():
 	center = get_viewport_rect().size/2
+	target = owner.get_node("Player")
+	position = target.global_position
+	zoom = Vector2(.3,.3)
 	
 func _process(delta):
 	if Input.is_action_just_released("zoom"):
@@ -18,9 +21,13 @@ func _process(delta):
 			zoomed = true
 	
 	if zoomed:
-		zoom = zoom.move_toward(Vector2(0.3,0.3), 0.03)
-		position = position.move_toward(target.global_position,70)
+		position = target.global_position
+		zoom = Vector2(.3,.3)
+		#zoom = zoom.move_toward(Vector2(0.3,0.3), 0.03)
+		#position = position.move_toward(target.global_position,70)
 	
 	else:
-		zoom = zoom.move_toward(Vector2(1,1), 0.03)
-		position = position.move_toward(center,70)
+		position = center
+		zoom = Vector2(1,1)
+		#zoom = zoom.move_toward(Vector2(1,1), 0.03)
+		#position = position.move_toward(center,70)
