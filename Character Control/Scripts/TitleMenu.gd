e6e5f809fa2e90b4681645d3180d0c6c291e013e
riltho1 <100237088+riltho1@users.noreply.github.com>
2022-09-07 12:25:55 +1200

    TitleMenuNearlyFinshed

--- Character Control/Scripts/TitleMenu.gd
+++ Character Control/Scripts/TitleMenu.gd
@@ -3,3 +3,12 @@ extends Control
 
 func _ready():
 	pass
+
+
+func _on_Options_pressed():
+	$AnimationPlayer.play("MoveToOptions")
+	
+
+
+func _on_Back_pressed():
+	$AnimationPlayer.play("MoveToMain")
