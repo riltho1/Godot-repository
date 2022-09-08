b33cd191e6de3d4ba7edaabece8094428a0ad8db
riltho1 <100237088+riltho1@users.noreply.github.com>
2022-09-08 13:25:48 +1200

    Menu_Finished

--- Character Control/Scripts/TitleMenu.gd
+++ Character Control/Scripts/TitleMenu.gd
@@ -7,8 +7,19 @@ func _ready():
 
 func _on_Options_pressed():
 	$AnimationPlayer.play("MoveToOptions")
-	
-
 
 func _on_Back_pressed():
 	$AnimationPlayer.play("MoveToMain")
+
+func _on_SFXSlider_value_changed(value):
+	SoundPlayer.change_sound_volume(value)
+
+func _on_MusicSlider_value_changed(value):
+	SoundPlayer.change_music_volume(value)
+
+
+func _on_CheckBox_toggled(button_pressed):
+	if button_pressed:
+		OS.window_fullscreen = true
+	else:
+		OS.window_fullscreen = false
--- Character Control/Scripts/sound_player.gd
+++ Character Control/Scripts/sound_player.gd
@@ -1,9 +1,9 @@
 extends Node
 
-onready var music = AudioStreamPlayer.new()
+onready var music_player = AudioStreamPlayer.new()
 
 var music_tracks = {
-	"title_track":"res://sfx/music/happywalking.wav",
+	"title_track":"res://sfx/music/Spiral - Matt J Hart (MP3).mp3",
 	"main":"res://sfx/music/happywalking.wav",
 }
 var sound_effects = {
@@ -14,22 +14,28 @@ var sound_effects = {
 var music_db = 1
 var sound_db = 1
 
-func change_music_db(val):
-	music_db = linear2db(val)
+func change_music_volume(value):
+	music_db = linear2db(value)
+	music_player.volume_db = music_db
 
-func change_sound_db(val):
-	sound_db = linear2db(val)
+func change_sound_volume(value):
+	sound_db = linear2db(value)
 
 
 func _ready():
-	music.stream = load(music_tracks["main"])
-	add_child(music)
-	music.play()
-	
+	music_player.stream = load(music_tracks["main"])
+	add_child(music_player)
+	music_player.play()
+
+func chang_song(track):
+	music_player.stream = load (music_tracks[track])
+	music_player.play()
+
 func play_sound_effects(sfx):
 	var sound = AudioStreamPlayer.new()
 	sound.stream = load(sound_effects[sfx])
 	add_child(sound)
+	sound.volume_db = sound_db
 	sound.play()
 	yield(sound,"finished")
 	sound.queue_free()
