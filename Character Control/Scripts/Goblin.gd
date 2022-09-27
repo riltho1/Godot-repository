4649891673e01e4c941e0101845b111d5c2ae5ef
riltho1 <100237088+riltho1@users.noreply.github.com>
2022-09-27 14:59:06 +1300

    Adding new enemies/traps

---
+++ Character Control/Scripts/Goblin.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+
+func _ready():
+	$AnimationPlayer.play("Running")
+
+func _on_Area2D_body_entered(body):
+	print(body)
+	if body.is_in_group("Player"):
+		print("player dead")
+		if GameStats.check_reset() == false:
+			body.global_position = GameStats.get_spawn().global_position
